df3ea14f80ad522fa6d673b40aa6f545f6af50d9
hammy275 <hammy275@gmail.com>
2024-03-11 21:13:22 -0400

    Lightning Strike for Snap Handler

--- common/src/main/java/com/hammy275/vrhandcontrol/common/vr/handlers/SnapHandler.java
+++ common/src/main/java/com/hammy275/vrhandcontrol/common/vr/handlers/SnapHandler.java
@@ -5,13 +5,25 @@ import com.hammy275.vrhandcontrol.client.vr_data.vd.Finger;
 import com.hammy275.vrhandcontrol.client.vr_data.vd.Hand;
 import com.hammy275.vrhandcontrol.client.vr_data.vd.Joint;
 import com.hammy275.vrhandcontrol.client.vr_data.vd.VRData;
+import com.hammy275.vrhandcontrol.client.vr_data.vivecraft.VRPlugin;
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import org.joml.Vector3f;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class SnapHandler implements GestureHandler {
 
@@ -36,7 +48,42 @@ public class SnapHandler implements GestureHandler {
 
     @Override
     public boolean performAction(Player player) {
-        player.sendSystemMessage(Component.literal("Snap detected!"));
+        BlockPos blockTarget = null;
+        LivingEntity entityTarget = null;
+        HitResult hit = player.pick(50d, 1f, false);
+        if (hit instanceof BlockHitResult bhr && bhr.getType() == HitResult.Type.BLOCK) {
+            blockTarget = bhr.getBlockPos();
+        }
+        // Not-so-great raycast for entity detection
+        Vec3 pos = VRPlugin.API.getVRPlayer(player).getHMD().position();
+        Vec3 look = VRPlugin.API.getVRPlayer(player).getHMD().getLookAngle();
+        for (int i = 0; i < 50; i++) {
+            List<Entity> nearby = player.level().getEntities(player, AABB.ofSize(pos.add(look.scale(i)), 4, 4, 4),
+                    (e) -> e instanceof LivingEntity);
+            if (!nearby.isEmpty()) {
+                entityTarget = (LivingEntity) nearby.get(0);
+                break;
+            }
+        }
+        if (blockTarget == null && entityTarget == null) {
+            // TODO: Use translation string here.
+            player.sendSystemMessage(Component.literal("No valid targets!"));
+            return false;
+        }
+        boolean hitEntity = blockTarget == null || (entityTarget != null && player.distanceToSqr(entityTarget) < player.distanceToSqr(blockTarget.getX(), blockTarget.getY(), blockTarget.getZ()));
+
+        LightningBolt bolt = new LightningBolt(EntityType.LIGHTNING_BOLT, player.level());
+        bolt.setVisualOnly(true);
+        if (hitEntity) {
+            bolt.setPos(entityTarget.position());
+            DamageSource source = entityTarget.isInvulnerableTo(entityTarget.damageSources().lightningBolt()) ?
+                    entityTarget.damageSources().genericKill() : entityTarget.damageSources().lightningBolt();
+            entityTarget.hurt(source, entityTarget.getHealth() * 4f);
+        } else {
+            bolt.setPos(Vec3.atBottomCenterOf(blockTarget).add(0, 1, 0));
+        }
+        player.level().addFreshEntity(bolt);
+
         return true;
     }
 
